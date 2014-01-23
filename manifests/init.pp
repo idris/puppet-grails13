f72b7ba10b57800f2ede38fc0824e1430845ffbf
Idris Mokhtarzada <idris.m@gmail.com>
2014-01-23 16:34:05 -0500

    first try

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,16 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Grails 1.3.7
+#
+# Usage:
+#
+#   include grails13
+class grails13 {
+  include homebrew
+
+  homebrew::formula { 'grails13':
+    before => Package['boxen/brews/grails'],
+  }
+
+  package { 'boxen/brews/grails':
+    ensure => '1.3.7-boxen1'
+  }
 }
